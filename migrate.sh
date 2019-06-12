ca93f8f8f6604738464cd056857b14f27818d531
Peter Droogmans (attiks) <peter@attiks.com>
2019-06-12 14:25:26 +0200

    Migrate other field collection and paragraphs

--- migrate.sh
+++ migrate.sh
@@ -15,6 +15,24 @@ fin drush mim --verbose upgrade_d7_user
 
 fin drush mim --verbose upgrade_d7_node_oa_wiki_page
 fin drush mim --verbose upgrade_d7_node_contact
+
+# Field collection to paragraphs
 fin drush mim --verbose agenda_items
+fin drush mim --verbose additional_locations
+fin drush mim --verbose membership
+
+# Paragraphs to paragraphs
+fin drush mim --verbose paragraph_media
+fin drush mim --verbose paragraph_content
+fin drush mim --verbose paragraph_snippet
+fin drush mim --verbose paragraph_text
 
 fin drush mim --verbose upgrade_d7_node_oa_event
+fin drush mim --verbose upgrade_d7_node_action_point
+fin drush mim --verbose upgrade_d7_node_announcement
+fin drush mim --verbose upgrade_d7_node_oa_discussion_post
+fin drush mim --verbose upgrade_d7_node_oa_event
+fin drush mim --verbose upgrade_d7_node_oa_ical_importer
+fin drush mim --verbose upgrade_d7_node_oa_worktracker_task
+fin drush mim --verbose upgrade_d7_node_oa_team
+fin drush mim --verbose upgrade_d7_node_oa_section
