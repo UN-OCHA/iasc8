0dea3819001a64b90af1efa4072b39de12c74dc3
Peter Droogmans (attiks) <peter@attiks.com>
2019-06-18 09:26:31 +0200

    Remove group from mim

--- migrate.sh
+++ migrate.sh
@@ -12,7 +12,10 @@ fin drush mim --verbose upgrade_d7_taxonomy_term_product_categories
 fin drush mim --verbose upgrade_d7_taxonomy_term_section_type
 fin drush mim --verbose upgrade_d7_taxonomy_term_space_type
 
-fin drush mim --verbose --group=oa_group_space
+#fin drush mim --verbose --group=oa_group_space
+fin drush mim --verbose upgrade_d7_node_oa_group
+fin drush mim --verbose upgrade_d7_node_oa_space
+
 fin drush mim --verbose upgrade_d7_user_role
 fin drush mim --verbose upgrade_d7_user
 
