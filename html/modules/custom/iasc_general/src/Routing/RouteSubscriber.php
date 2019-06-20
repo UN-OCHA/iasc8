487ac2d1ec73965c2d91b269b67e08b76e0c5bf5
Peter Droogmans (attiks) <peter@attiks.com>
2019-06-20 15:44:02 +0200

    IASC-558: Override user login

---
+++ html/modules/custom/iasc_general/src/Routing/RouteSubscriber.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\iasc_general\Routing\RouteSubscriber.
+ */
+
+namespace Drupal\iasc_general\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Listens to the dynamic route events.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+    if ($route = $collection->get('user.login')) {
+      $route->setPath('/user/login/hid');
+    }
+  }
+}
