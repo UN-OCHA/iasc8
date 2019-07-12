4fa7273f08199465edc034dece2dc05550e4e00c
Peter Droogmans (attiks) <peter@attiks.com>
2019-07-12 09:56:41 +0200

    IASC-562: More tests

---
+++ html/modules/custom/iasc_content/tests/src/ExistingSite/ContactsNotVisibleForAnonTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\Tests\iasc_content\ExistingSite;
+
+use Drupal\user\Entity\User;
+use weitzman\DrupalTestTraits\ExistingSiteBase;
+use weitzman\LoginTrait\LoginTrait;
+
+/**
+ * Make sure contacts are not accessible by anonymous user.
+ */
+class ContactsNotVisibleForAnonTest extends ExistingSiteBase {
+  use LoginTrait;
+
+  /**
+   * Make sure contacts are not accessible by anonymous user.
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   * @throws \Drupal\Core\Entity\EntityMalformedException
+   * @throws \Behat\Mink\Exception\ExpectationException
+   */
+  public function testContactForAnon() {
+    $admin = User::load(1);
+
+    // Create a contact.
+    $node = $this->createNode([
+      'title' => 'Test contact',
+      'type' => 'contact',
+      'uid' => $admin->id(),
+    ]);
+    $node->setPublished()->save();
+
+    // We can not browse the contact.
+    $this->drupalGet($node->toUrl());
+    $this->assertSession()->statusCodeEquals(403);
+
+    // Login using reset link.
+    $this->drupalLogin($admin);
+
+    // We can browse the contact as admin.
+    $this->drupalGet($node->toUrl());
+    $this->assertSession()->statusCodeEquals(200);
+  }
+
+}
