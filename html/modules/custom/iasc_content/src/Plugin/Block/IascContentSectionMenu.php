331c0760effc631ad0cdf61bf0fb6c19faa1d7cb
Peter Droogmans (attiks) <peter@attiks.com>
2019-11-18 11:05:12 +0100

    IASC-580: Use dependency injection

--- html/modules/custom/iasc_content/src/Plugin/Block/IascContentSectionMenu.php
+++ html/modules/custom/iasc_content/src/Plugin/Block/IascContentSectionMenu.php
@@ -3,7 +3,10 @@
 namespace Drupal\iasc_content\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\group\Entity\Group;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a 'Section menu' Block.
@@ -14,13 +17,53 @@ use Drupal\group\Entity\Group;
  *   category = @Translation("IASC blocks"),
  * )
  */
-class IascContentSectionMenu extends BlockBase {
+class IascContentSectionMenu extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The route match.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatch;
+
+  /**
+   * Constructs a new instance.
+   *
+   * @param array $configuration
+   *   The plugin configuration, i.e. an array with configuration values keyed
+   *   by configuration option name. The special key 'context' may be used to
+   *   initialize the defined contexts by setting it to an array of context
+   *   values keyed by context names.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The route match.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, RouteMatchInterface $route_match) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->routeMatch = $route_match;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('current_route_match')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
-    $parameters = \Drupal::routeMatch()->getParameters();
+    $parameters = $this->routeMatch->getParameters();
 
     if (($parameters->get('view_id', '') == 'iasc_group_content') && ($group_id = $parameters->get('group', ''))) {
       $group = Group::load($group_id);
