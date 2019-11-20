40017d038c4250564c1a34572457ed232ada0130
Peter Droogmans (attiks) <peter@attiks.com>
2019-11-20 11:36:30 +0100

    IASC-592: Hard coded header search

---
+++ html/modules/custom/iasc_content/src/Plugin/Block/IascHeaderSearch.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Drupal\iasc_content\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a 'Header search' Block.
+ *
+ * @Block(
+ *   id = "iasc_header_search_block",
+ *   admin_label = @Translation("Header search"),
+ *   category = @Translation("IASC blocks"),
+ * )
+ */
+class IascHeaderSearch extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The route match.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatch;
+
+  /**
+   * Constructs a new instance.
+   *
+   * @param array $configuration
+   *   The plugin configuration, i.e. an array with configuration values keyed
+   *   by configuration option name. The special key 'context' may be used to
+   *   initialize the defined contexts by setting it to an array of context
+   *   values keyed by context names.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The route match.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, RouteMatchInterface $route_match) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->routeMatch = $route_match;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('current_route_match')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $parameters = $this->routeMatch->getParameters();
+
+    return [
+      '#theme' => 'iasc_header_search_block',
+      '#title' => '',
+      '#keyword' => '',
+      '#cache' => [
+        'contexts' => ['url'],
+      ],
+    ];
+  }
+
+}
