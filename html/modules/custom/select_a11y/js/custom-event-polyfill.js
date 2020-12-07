19a2bb9f85407ad45f74fc2b3290bdb718b214e2
left23 <lcdemc@gmail.com>
2020-12-07 14:51:35 +0100

    CS

--- html/modules/custom/select_a11y/js/custom-event-polyfill.js
+++ html/modules/custom/select_a11y/js/custom-event-polyfill.js
@@ -2,11 +2,13 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent#Polyfill
 (function () {
 
-  if ( typeof window.CustomEvent === "function" ) return false;
+  if (typeof window.CustomEvent === "function") {
+    return false;
+  }
 
-  function CustomEvent ( event, params ) {
+  function CustomEvent (event, params) {
     params = params || { bubbles: false, cancelable: false, detail: null };
-    var evt = document.createEvent( 'CustomEvent' );
+    var evt = document.createEvent('CustomEvent');
     evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
     return evt;
   }
