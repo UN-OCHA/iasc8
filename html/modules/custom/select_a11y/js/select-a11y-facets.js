ddc4d975055985017d206c17adc78e2b74491e9f
Peter Droogmans (attiks) <peter@attiks.com>
2020-12-14 16:18:26 +0100

    Delay select a11y

--- html/modules/custom/select_a11y/js/select-a11y-facets.js
+++ html/modules/custom/select_a11y/js/select-a11y-facets.js
@@ -3,28 +3,29 @@
 
   Drupal.behaviors.selectA11yFacet = {
     attach: function (context, settings) {
-      $('select[data-select-a11y]').once('facets-dropdown-select-a11y').each(function () {
-        // Add change handler.
-        $(this).on('change', function (e) {
-          var url = this.options[e.detail].value;
-          window.location = url;
+      window.setTimeout(function (context, settings) {
+        $('select[data-select-a11y]').once('facets-dropdown-select-a11y').each(function () {
+          // Add change handler.
+          $(this).on('change', function (e) {
+            var url = this.options[e.detail].value;
+            window.location = url;
+          });
+
+          // Get Id.
+          var id = $(this).data('drupal-facet-id');
+
+          new Select(this, {
+            text: {
+              help: Drupal.t('Navigate the list to find what you are looking for'),
+              placeholder: settings.facets.dropdown_widget[id]['facet-default-option-label'] || Drupal.t('Choose'),
+              noResult: Drupal.t('Nothing found'),
+              results: Drupal.t('{x} suggestion(s) found'),
+              deleteItem: Drupal.t('Remove {t}'),
+              delete: Drupal.t('Remove')
+            }
+          })
         });
-
-        // Get Id.
-        var id = $(this).data('drupal-facet-id');
-
-        // TODO: Only on desktop?
-        new Select(this, {
-          text: {
-            help: Drupal.t('Navigate the list to find what you are looking for'),
-            placeholder: settings.facets.dropdown_widget[id]['facet-default-option-label'] || Drupal.t('Choose'),
-            noResult: Drupal.t('Nothing found'),
-            results: Drupal.t('{x} suggestion(s) found'),
-            deleteItem: Drupal.t('Remove {t}'),
-            delete: Drupal.t('Remove')
-          }
-        })
-      });
+      }.bind(this, context, settings), 200);
     }
   };
 })(jQuery, Drupal);
--- html/modules/custom/select_a11y/js/select-a11y-widget.js
+++ html/modules/custom/select_a11y/js/select-a11y-widget.js
@@ -3,18 +3,20 @@
 
   Drupal.behaviors.selectA11y = {
     attach: function (context, settings) {
-      $('select.select-a11y-widget').once('once-select-a11y').each(function () {
-        new Select(this, {
-          text: {
-            help: Drupal.t('Navigate the list to find what you are looking for'),
-            placeholder: Drupal.t('Choose'),
-            noResult: Drupal.t('Nothing found'),
-            results: Drupal.t('{x} suggestion(s) found'),
-            deleteItem: Drupal.t('Remove {t}'),
-            delete: Drupal.t('Remove')
-          }
-        })
-      });
+      setTimeout(function (context, settings) {
+        $('select.select-a11y-widget').once('once-select-a11y').each(function () {
+          new Select(this, {
+            text: {
+              help: Drupal.t('Navigate the list to find what you are looking for'),
+              placeholder: Drupal.t('Choose'),
+              noResult: Drupal.t('Nothing found'),
+              results: Drupal.t('{x} suggestion(s) found'),
+              deleteItem: Drupal.t('Remove {t}'),
+              delete: Drupal.t('Remove')
+            }
+          })
+        });
+      }.bind(this, context, settings), 200);
     }
   };
 })(jQuery, Drupal);
