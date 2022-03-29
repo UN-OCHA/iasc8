7ead043bfddb221b41fe98ddd9ee96af946d4a83
Peter Droogmans (attiks) <peter@attiks.com>
2022-03-29 16:41:29 +0200

    cs

--- html/modules/custom/iasc_content/src/Form/IascContentConfigForm.php
+++ html/modules/custom/iasc_content/src/Form/IascContentConfigForm.php
@@ -6,7 +6,7 @@ use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
- * Class IascContentConfigForm.
+ * Iasc settings form.
  */
 class IascContentConfigForm extends ConfigFormBase {
 
