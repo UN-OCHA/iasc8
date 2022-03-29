99016b017ec031e18a44b237c67c904266bf8467
Peter Droogmans (attiks) <peter@attiks.com>
2022-03-29 15:14:14 +0200

    feat: Add config form
    Refs: IASC-729

---
+++ html/modules/custom/iasc_content/src/Form/IascContentConfigForm.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\iasc_content\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class IascContentConfigForm.
+ */
+class IascContentConfigForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'iasc_content.settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'iasc_content_config_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('iasc_content.settings');
+    $form['archive_message'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Archive message'),
+      '#default_value' => $config->get('archive_message'),
+    ];
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    parent::submitForm($form, $form_state);
+
+    $this->config('iasc_content.settings')
+      ->set('archive_message', $form_state->getValue('archive_message'))
+      ->save();
+  }
+
+}
