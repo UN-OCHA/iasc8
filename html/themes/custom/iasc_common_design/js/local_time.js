592a5070af6c300368c7cb2c5a3684e1f7fee041
Peter Droogmans (attiks) <peter@attiks.com>
2022-07-13 17:05:06 +0200

    bug: local time
    Refs: #IASC-753

---
+++ html/themes/custom/iasc_common_design/js/local_time.js
@@ -0,0 +1,36 @@
+/**
+ * @file
+ * Display time in local timezone.
+ */
+
+(function () {
+  var spans = document.querySelectorAll('.cd-teaser__date__time-user[data-timestamp-start]');
+  if (spans) {
+    spans.forEach(function (span) {
+      let start = new Date(parseInt(span.getAttribute('data-timestamp-start'), 10) * 1000);
+      let end = new Date(parseInt(span.getAttribute('data-timestamp-end'), 10) * 1000);
+
+      span.innerHTML = '<br>';
+
+      let hours = start.getHours();
+      let ampm = 'am';
+      if (hours >= 12 ) {
+        ampm = 'pm';
+      }
+      hours = hours % 12;
+      hours = hours ? hours : 12;
+      span.innerHTML += hours.toString().padStart(2, '0') + '.' + start.getMinutes().toString().padStart(2, '0') + ' ' + ampm;
+
+      hours = end.getHours();
+      ampm = 'am';
+      if (hours >= 12 ) {
+        ampm = 'pm';
+      }
+      hours = hours % 12;
+      hours = hours ? hours : 12;
+      span.innerHTML += ' — ' + hours.toString().padStart(2, '0') + '.' + end.getMinutes().toString().padStart(2, '0');
+      span.innerHTML += ' (' + Intl.DateTimeFormat().resolvedOptions().timeZone + ')';
+    });
+  }
+}());
+
