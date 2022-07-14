8104de2afe335761fc1fa5463254b9fbf530a5c3
Peter Droogmans (attiks) <peter@attiks.com>
2022-07-14 08:55:49 +0200

    bug: display date from teaser
    Refs: #IASC-753

--- html/themes/custom/iasc_common_design/js/local_time.js
+++ html/themes/custom/iasc_common_design/js/local_time.js
@@ -32,5 +32,20 @@
       span.innerHTML += ' (' + Intl.DateTimeFormat().resolvedOptions().timeZone + ')';
     });
   }
+
+  // Check query string.
+  if (window.location.search.length > 0) {
+    const searchParams = new URLSearchParams(window.location.search);
+    if (searchParams.has('ts')) {
+      const month_names_short = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+      let container = document.querySelector('.node__content .cd-date');
+      let date = new Date(parseInt(searchParams.get('ts'), 10) * 1000);
+
+      container.querySelector('.cd-date__day').innerHTML = date.getDate();
+      container.querySelector('.cd-date__month').innerHTML = month_names_short[date.getMonth()];
+      container.querySelector('.cd-date__year').innerHTML = date.getFullYear();
+    }
+  }
 }());
 
