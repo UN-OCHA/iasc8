794c193d843f48709dcbf4d902b434ffa573c17d
Peter Droogmans (attiks) <peter@attiks.com>
2022-07-19 16:03:42 +0200

    feat: multi day event
    Refs: #IASC-753

--- html/themes/custom/iasc_common_design/js/local_time.js
+++ html/themes/custom/iasc_common_design/js/local_time.js
@@ -12,10 +12,26 @@
 
       let container = document.querySelector('.node__content .cd-date');
       let date = new Date(parseInt(searchParams.get('ts'), 10) * 1000);
+      let date_end = new Date(parseInt(searchParams.get('tse'), 10) * 1000);
 
-      container.querySelector('.cd-date__day').innerHTML = date.getDate();
-      container.querySelector('.cd-date__month').innerHTML = month_names_short[date.getMonth()];
-      container.querySelector('.cd-date__year').innerHTML = date.getFullYear();
+      let day = date.getDate();
+      if (date.getDate() != date_end.getDate()) {
+        day = day + ' - ' + date_end.getDate();
+      }
+
+      let month = month_names_short[date.getMonth()];
+      if (date.getMonth() != date_end.getMonth()) {
+        month = month + ' - ' + month_names_short[date_end.getDate()];
+      }
+
+      let year = date.getFullYear();
+      if (date.getFullYear() != date_end.getFullYear()) {
+        year = year + ' - ' + date_end.getFullYear();
+      }
+
+      container.querySelector('.cd-date__day').innerHTML = day;
+      container.querySelector('.cd-date__month').innerHTML = month;
+      container.querySelector('.cd-date__year').innerHTML = year;
     }
   }
 }());
