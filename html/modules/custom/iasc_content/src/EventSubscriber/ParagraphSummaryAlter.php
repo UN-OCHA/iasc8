c0e958ad160b67b296ac6c2d17a36b43fe817d1b
Peter Droogmans (attiks) <peter@attiks.com>
2023-10-11 14:43:13 +0200

    CS

--- html/modules/custom/iasc_content/src/EventSubscriber/ParagraphSummaryAlter.php
+++ html/modules/custom/iasc_content/src/EventSubscriber/ParagraphSummaryAlter.php
@@ -3,7 +3,6 @@
 namespace Drupal\iasc_content\EventSubscriber;
 
 use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\views\Views;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
@@ -31,83 +30,7 @@ class ParagraphSummaryAlter implements EventSubscriberInterface {
    * {@inheritdoc}
    */
   public static function getSubscribedEvents() {
-    return [
-    ];
-  }
-
-  /**
-   * Alter paragraph summary.
-   *
-   * @param \Drupal\paragraphs\Event\ParagraphSummaryAlterEvent $event
-   *   Event object.
-   */
-  public function onSummaryAlter(ParagraphSummaryAlterEvent $event) {
-    if ($event->getFieldType() == 'viewsreference') {
-      $paragraph = $event->getParagraph();
-      $item = $paragraph->get($event->getFieldName())->first();
-      if (!$item) {
-        return;
-      }
-
-      $view_name = $item->getValue()['target_id'];
-
-      if (!$view_name) {
-        return;
-      }
-
-      $view = Views::getView($view_name);
-      $view_label = $view->storage->label();
-
-      $display_id = $item->getValue()['display_id'];
-      $view->setDisplay($display_id);
-      $display_label = $view->getDisplay()->display['display_title'];
-
-      $event->appendTextToSummary($view_label . ' -- ' . $display_label);
-
-      $data = unserialize($item->getValue()['data'], [
-        'allowed_classes' => FALSE,
-      ]);
-      if (is_array($data)) {
-        foreach ($data as $key => $value) {
-          if (!empty($value)) {
-            if (is_array($value)) {
-              if ($key == 'ocha_exposed_filters') {
-                $tids = [];
-                foreach ($value as $filter_values) {
-                  if (!is_array($filter_values)) {
-                    if (!empty($filter_values)) {
-                      $filter_values = [$filter_values];
-                    }
-                    else {
-                      $filter_values = [];
-                    }
-                  }
-                  elseif (isset($filter_values[0]['target_id'])) {
-                    $filter_values = array_column($filter_values, 'target_id');
-                  }
-                  $tids = array_merge($tids, $filter_values);
-                }
-
-                $terms = $this->entityTypeManager->getStorage('taxonomy_term')->loadMultiple($tids);
-                $labels = [];
-                /** @var \Drupal\taxonomy\Entity\Term $term */
-                foreach ($terms as $term) {
-                  $labels[] = $term->getName();
-                }
-
-                $event->appendTextToSummary('Filter(s): ' . implode(', ', $labels));
-              }
-              else {
-                $event->appendTextToSummary($key . ': ' . reset($value));
-              }
-            }
-            else {
-              $event->appendTextToSummary($key . ': ' . $value);
-            }
-          }
-        }
-      }
-    }
+    return [];
   }
 
 }
