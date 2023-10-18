00d73b71081a5af2184f2201708f7837aada958f
Peter Droogmans (attiks) <peter@attiks.com>
2023-10-18 14:56:39 +0200

    feat: Convert private files to public and add redirect
    Refs: #IASC-780

--- html/modules/custom/iasc_content/iasc_content.post_update.php
+++ html/modules/custom/iasc_content/iasc_content.post_update.php
@@ -5,6 +5,9 @@
  * Post update functions for iasc content.
  */
 
+use Drupal\Core\File\FileSystemInterface;
+use Drupal\redirect\Entity\Redirect;
+
 /**
  * Update grand bargain flag.
  */
@@ -22,5 +25,69 @@ function iasc_content_post_update_grand_bargain_for_announcements() {
 
     $node->set('field_grand_bargain_content', TRUE);
     $node->save();
+  }
+}
+
+/**
+ * Convert private files to public.
+ */
+function iasc_content_post_update_private_files_to_public(array $sandbox) {
+  $new_file_dir = 'public://private/';
+
+  if (!isset($sandbox['total'])) {
+    $sandbox['total'] = \Drupal::entityTypeManager()
+      ->getStorage('file')
+      ->getQuery()
+      ->accessCheck(FALSE)
+      ->condition('uri', 'private://', 'STARTS_WITH')
+      ->count()->execute();
+
+    $sandbox['current'] = 0;
+
+    if (empty($sandbox['total'])) {
+      $sandbox['#finished'] = 1;
+      return;
+    }
+  }
+
+  // Load 50 files.
+  $file_entity_ids = \Drupal::entityTypeManager()
+    ->getStorage('file')
+    ->getQuery()
+    ->accessCheck(FALSE)
+    ->condition('uri', 'private://', 'STARTS_WITH')
+    ->range(0, 50)
+    ->execute();
+
+  if (empty($file_entities)) {
+    $sandbox['#finished'] = 1;
+    return;
+  }
+
+  // Load the file entities.
+  $file_entities = \Drupal::entityTypeManager()
+    ->getStorage('file')
+    ->loadMultiple($file_entity_ids);
+
+  // Move the files associated with the file entities.
+  /** @var \Drupal\file\Entity\File $file */
+  foreach ($file_entities as $file) {
+    $new_file_dir = 'public://migrated/' . date('Y-m', $file->getCreatedTime()) . '/';
+    \Drupal::service('file_system')->prepareDirectory($new_file_dir, FileSystemInterface::CREATE_DIRECTORY);
+
+    // No slash at the start.
+    $source_url = ltrim($file->createFileUrl(), '/');
+    $file = \Drupal::service('file.repository')->move($file, $new_file_dir . $file->getFilename());
+
+    // Avoid double encode.
+    $destination = 'internal:' . urldecode($file->createFileUrl());
+
+    // Add a redirect.
+    Redirect::create([
+      'redirect_source' => $source_url,
+      'redirect_redirect' => $destination,
+      'language' => 'und',
+      'status_code' => '301',
+    ])->save();
   }
 }
